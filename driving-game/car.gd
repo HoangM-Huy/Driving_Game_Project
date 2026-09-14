349956c57b8f5d6c1620ef42c30ee0fd98d4603b
QBBs24 <quocbao3560@gmail.com>
2026-09-14 21:26:30 +0700

    Create prototype Sedan
    Created a prototype Sedan that can drive and has a 3D Chasing camera

---
+++ driving-game/car.gd
@@ -0,0 +1,21 @@
+extends VehicleBody3D
+
+func _ready() -> void:
+	var frontLeft = $Pivot/Sedan/FrontLeft
+	var frontRight = $Pivot/Sedan/FrontRight
+	var rearLeft = $Pivot/Sedan/RearLeft
+	var rearRight = $Pivot/Sedan/RearRight
+	
+	frontLeft.reparent($FrontLeft)
+	frontRight.reparent($FrontRight)
+	rearLeft.reparent($RearLeft)
+	rearRight.reparent($RearRight)
+	
+	frontLeft.position = Vector3.ZERO
+	frontRight.position = Vector3.ZERO
+	rearLeft.position = Vector3.ZERO
+	rearRight.position = Vector3.ZERO
+
+func _physics_process(_delta: float) -> void:
+	steering = Input.get_axis("move_right", "move_left") * 0.4
+	engine_force = Input.get_axis("forward", "reverse") * 700
