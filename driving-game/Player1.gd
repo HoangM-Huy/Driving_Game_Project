375017a47d7a969c94bc1715cef7f0175dd79671
HoangM-Huy <huyhoang6800@gmail.com>
2026-09-20 22:12:53 +0700

    Phase 1 (Kinda complete)

--- driving-game/Player1.gd
+++ driving-game/Player1.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 # How fast the player moves in meters per second.
-@export var speed = 14
+@export var speed = 5
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
@@ -74,3 +74,13 @@ func _input(event: InputEvent):
 	# Mouse movement
 	if event is InputEventMouseMotion and LockMouse: 
 		mouseInput += event.relative * 0.01
+		
+func enter_car():
+	set_physics_process(false)
+	hide()
+	camera.current = false
+	
+func leave_car():
+	set_physics_process(true)
+	show()
+	camera.current = true
--- driving-game/car.gd
+++ driving-game/car.gd
@@ -1,5 +1,31 @@
 extends VehicleBody3D
 
+@onready var input_label: Label3D = $InputLabel
+
+var withPlayer = false
+
+func _input(event: InputEvent) -> void:
+	var canEnter = input_label.visible and not withPlayer
+	var canLeave = withPlayer
+	
+	if Input.is_action_just_pressed("interaction") and canEnter:
+		_enter_car()
+	elif Input.is_action_just_pressed("interaction") and canLeave:
+		_leave_car()
+	
+func _enter_car():
+	withPlayer = true
+	
+	var player = get_tree().get_first_node_in_group("player")
+	player.enter_car()
+	
+func _leave_car():
+	withPlayer = false
+	var player = get_tree().get_first_node_in_group("player")
+	player.leave_car()
+	
+	player.global_position = global_position
+
 func _ready() -> void:
 	var frontLeft = $Pivot/Sedan/FrontLeft
 	var frontRight = $Pivot/Sedan/FrontRight
@@ -17,6 +43,17 @@ func _ready() -> void:
 	rearRight.position = Vector3.ZERO
 
 func _physics_process(_delta: float) -> void:
-	#steering = Input.get_axis("move_right", "move_left") * 0.4
-	#engine_force = Input.get_axis("move_forward", "move_back") * 700
+	if not withPlayer: return
+	steering = Input.get_axis("move_right", "move_left") * 0.4
+	engine_force = Input.get_axis("move_forward", "move_back") * 1000
 	pass
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		input_label.show()
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		input_label.hide()
